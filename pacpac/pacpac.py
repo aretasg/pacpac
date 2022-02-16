75d16808a6484990451c160b1d3e364c346cf84c
aretasg <gasp@Aretass-MacBook-Pro.local>
2022-02-16 01:08:57 +0000

    function to proble multiple sequences

--- pacpac/pacpac.py
+++ pacpac/pacpac.py
@@ -927,3 +927,349 @@ def probe(
     print("Another happy probing")
 
     return df
+
+
+def probe_multiple(
+    probe_df: pd.DataFrame,
+    df: pd.DataFrame,
+    vh_aa_sequence_col_name: str,
+    vl_aa_sequence_col_name: Optional[str] = None,
+    scheme: Optional[str] = "imgt",
+    cdr_scheme: Optional[str] = "chothia",
+    num_extra_residues: Optional[int] = 2,
+    paratope_residue_threshold: Optional[float] = 0.67,
+    paratope_identity_threshold: Optional[float] = 0.75,
+    clonotype_identity_threshold: Optional[float] = 0.72,
+    structural_equivalence: Optional[bool] = True,
+    perform_clonotyping: Optional[bool] = True,
+    perform_paratyping: Optional[bool] = True,
+    tokenize: Optional[bool] = False,
+    allowed_species: Optional[List[str]] = None,
+    cpu_count: Optional[int] = NB_WORKERS
+) -> pd.DataFrame:
+
+    """
+    Probe sequences in the pandas dataframe for similar paratopes (single chain only or both chains)
+    and clonotypes (single chain only).
+
+    Parameters
+    ----------
+    probe_df : pd.DataFrame
+        probe sequences.
+    df : pd.DataFrame
+        pandas dataframe to probe.
+    vh_aa_sequence_col_name : str
+        column name for VH sequences to probe.
+    vl_aa_sequence_col_name : str, None
+        column name for VL sequences to probe.
+    scheme : str, default "imgt"
+        numbering scheme to use.
+    cdr_scheme : str, default "chothia"
+        CDR definition to use. IMGT, North, Chothia and Contact supported.
+        IMGT, North can be used with IMGT numbering scheme.
+        North, Chothia, Contact can be used with Chothia, Martin numbering schemes.
+        CDR definition with not supported numbering scheme will default to the supported numbering scheme.
+    num_extra_residues : int, default 2
+        include extra residues at the start and end of each CDR.
+    paratope_residue_threshold : float, default 0.67
+        paratope residue call threshold for parapred predictions.
+    paratope_identity_threshold : float, default 0.75
+        paratope sequence identity value at which paratope are considered the same.
+    clonotype_identity_threshold : float, default 0.72
+        clonotype sequence identity value at which clonotype are considered the same.
+    structural_equivalence : bool, default True
+        specify whether structural equivalence as assigned by the numbering scheme of
+        choice should be used and paratopes with different CDR lengths to be compared.
+        Always defaults to True if clustering VH and VL pairs.
+    perform_clonotyping : bool, default True
+        specify if clonotyping should be performed.
+    tokenize : bool, default False
+        specify if residues should be tokenized to match residues of the same group when paratope probing.
+    allowed_species : List[str], default None
+        limit the species that can be assigned with ANARCI to a limited set. Useful when the
+        animal species is known or when performing closest germline experiments. Choose a subset of
+        ['human', 'mouse', 'rat', 'rabbit', 'rhesus', 'pig', 'alpaca']. Default is to use all species.
+    cpu_count : int, default - use all available CPU cores
+        specify how many CPU cores should be used for parallelizing processes
+
+    Returns
+    -------
+    pd.DataFrame
+    """
+
+    status = start_and_checks(scheme, cdr_scheme)
+    if status is False:
+        return None
+
+    both_chains = False
+    if vl_aa_sequence_col_name is not None:
+        both_chains = True
+        if structural_equivalence is False:
+            print("structural equivalence set to True")
+            structural_equivalence = True
+        print("Always two there are, no more, no less. A VH and a VL")
+
+    # annotations for the probes
+    final_probe_dict = {}
+    probe_df.reset_index(inplace=True)
+    for index, row in probe_df.iterrows():
+        probe_dict = annotate_sequence(
+            row[vh_aa_sequence_col_name],
+            scheme=scheme,
+            cdr_scheme=cdr_scheme,
+            assign_germline=perform_clonotyping,
+            num_extra_residues=num_extra_residues,
+            paratope_residue_threshold=paratope_residue_threshold,
+            structural_equivalence=structural_equivalence,
+            tokenize=tokenize,
+            allowed_species=allowed_species,
+        )
+        if both_chains:
+            vl_probe_dict = annotate_sequence(
+                row[vl_aa_sequence_col_name],
+                scheme=scheme,
+                cdr_scheme=cdr_scheme,
+                assign_germline=False,
+                num_extra_residues=num_extra_residues,
+                paratope_residue_threshold=paratope_residue_threshold,
+                structural_equivalence=True,
+                tokenize=tokenize,
+                allowed_species=allowed_species
+            )
+
+            # merge probe dicts
+            probe_dict["PARATOPE_DICT_REFORMAT"] = rename_dict_keys(
+                probe_dict["PARATOPE_DICT_REFORMAT"], "H"
+            )
+            vl_probe_dict["PARATOPE_DICT_REFORMAT"] = rename_dict_keys(
+                vl_probe_dict["PARATOPE_DICT_REFORMAT"], "L"
+            )
+            probe_dict["PARATOPE_DICT_REFORMAT"] = {
+                **probe_dict["PARATOPE_DICT_REFORMAT"],
+                **vl_probe_dict["PARATOPE_DICT_REFORMAT"],
+            }
+            probe_dict["PARATOPE_DICT_REFORMAT"] = convert_to_typed_numba_dict(
+                probe_dict["PARATOPE_DICT_REFORMAT"]
+            )
+        final_probe_dict[row['index']] = probe_dict
+
+    annotated_probe_df = pd.DataFrame.from_dict(final_probe_dict, orient='index')
+    annotated_probe_df.drop(
+        [
+            "CDR1_NUMBERING",
+            "CDR2_NUMBERING",
+            "CDR3_NUMBERING",
+            "PARATOPE_PROBS",
+            "PARATOPE_PROBS_NUMBERED",
+            "PARATOPE_DICT",
+            "PARATOPE_DICT_NUMBERED",
+            "PARATOPE_DICT_REFORMAT",
+            "LPARATOPE_DICT",
+            "LPARATOPE_DICT_NUMBERED",
+            "LPARATOPE",
+            "CHAIN_TYPE",
+        ],
+        axis=1,
+        inplace=True,
+        errors="ignore",
+    )
+    if both_chains:
+        annotated_probe_df.drop(
+            ["CDR1", "CDR2", "CDR3"],
+            axis=1,
+            inplace=True,
+            errors="ignore",
+    )
+    annotated_probe_df.to_csv('probes_annotated.csv')
+
+    # slicing rows where both VH and VL is NaN
+    if both_chains:
+        nan_df = df[
+            df[vh_aa_sequence_col_name].isnull() & df[vl_aa_sequence_col_name].isnull()
+        ]
+        df = df[
+            df[vh_aa_sequence_col_name].notnull()
+            & df[vl_aa_sequence_col_name].notnull()
+        ]
+    else:
+        nan_df = df[df[vh_aa_sequence_col_name].isnull()]
+        df = df[df[vh_aa_sequence_col_name].notnull()]
+    if not nan_df.empty:
+        print("Not to worry, we are still flying half a dataset")
+
+    # annotate sequences with genes and cdrs
+    print("I'll try annotating, that's a good trick")
+    init_pandarallel(nb_workers=cpu_count)
+    df = annotations_for_df(
+        df,
+        vh_aa_sequence_col_name,
+        assign_germline=perform_clonotyping,
+        scheme=scheme,
+        cdr_scheme=cdr_scheme,
+        num_extra_residues=num_extra_residues,
+        allowed_species=allowed_species
+    )
+
+    vl_df = None
+    if both_chains:
+        # annotating VL sequences
+        vl_df = annotations_for_df(
+            df[[vl_aa_sequence_col_name]],
+            vl_aa_sequence_col_name,
+            assign_germline=False,
+            scheme=scheme,
+            cdr_scheme=cdr_scheme,
+            num_extra_residues=num_extra_residues,
+            allowed_species=allowed_species
+        )
+
+        # exclude sequences where anarci has failed
+        nan_df2 = df[df["CDR3"].isnull() | vl_df["CDR3"].isnull()]
+        df = df[df["CDR3"].notnull() & vl_df["CDR3"].notnull()]
+        vl_df = vl_df[df["CDR3"].notnull() & vl_df["CDR3"].notnull()]
+    else:
+        nan_df2 = df[df["CDR3"].isnull()]
+        df = df[df["CDR3"].notnull()]
+
+    if df.empty is True:
+        raise ValueError('ANARCI failed to number the sequences. \
+            Make sure amino acid sequences are used as input!')
+
+    # probing with clonotype
+    if perform_clonotyping is True:
+
+        df["HCDR1_LEN"] = df["CDR1"].astype(str).map(len)
+        df["HCDR2_LEN"] = df["CDR2"].astype(str).map(len)
+        df["HCDR3_LEN"] = df["CDR3"].astype(str).map(len)
+
+        # exclude sequences 0 len for HCDR3
+        nan_df3 = df[df['HCDR3_LEN'].values <= num_extra_residues * 2]
+        df = df[df['HCDR3_LEN'].values > num_extra_residues * 2]
+
+        for index, probe_dict in final_probe_dict.items():
+            probe_cdr3_len = len(probe_dict["CDR3"]) - 2 * num_extra_residues
+            if probe_cdr3_len == 0:
+                raise ValueError("Probe CDR3 length cannot be zero")
+
+        print("We're just clonotypes, sir. We're meant to be expendable")
+
+        end_cdr = -1 * num_extra_residues
+        if num_extra_residues == 0:
+            end_cdr = None
+
+        for index, probe_dict in final_probe_dict.items():
+            df[f"CLONOTYPE_MATCH_{index}"] = [
+                check_clonotype(
+                    probe_dict["V_GENE"],
+                    probe_dict["J_GENE"],
+                    probe_cdr3_len,
+                    probe_dict["CDR3"][num_extra_residues:end_cdr],
+                    vh,
+                    jh,
+                    cdr3_len,
+                    cdr3_aa,
+                )
+                for vh, jh, cdr3_len, cdr3_aa in zip(
+                    df["V_GENE"],
+                    df["J_GENE"],
+                    (df["HCDR3_LEN"] - 2 * num_extra_residues),
+                    df["CDR3"].str[num_extra_residues:end_cdr]
+                )
+            ]
+
+        df = pd.concat(
+            [df, nan_df3], ignore_index=False, axis=0, sort=False
+            )
+
+    if perform_paratyping is True:
+
+        # create paratopes for sequences in df
+        print("Learning to stop worrying and falling in love with the paratope")
+
+        df, nan_df4 = paratopes_for_df_both_chains(
+            df,
+            vl_df,
+            both_chains=both_chains,
+            paratope_residue_threshold=paratope_residue_threshold,
+        )
+        df = tokenize_and_reformat(
+            df, structural_equivalence=structural_equivalence, tokenize=tokenize
+        )
+
+        # probing with paratope
+        print("This is where the paratope probing begins")
+        if structural_equivalence is False and both_chains is False:
+            for index, probe_dict in final_probe_dict.items():
+                df[f"PARATOPE_MATCH_{index}"] = [
+                    check_paratope_equal_len_cdrs(
+                        probe_dict["CDR1"],
+                        probe_dict["CDR2"],
+                        probe_dict["CDR3"],
+                        probe_dict["PARATOPE_LEN"],
+                        probe_dict["PARATOPE_DICT_REFORMAT"],
+                        cdr1,
+                        cdr2,
+                        cdr3,
+                        paratope_len,
+                        paratope,
+                    )
+                    for cdr1, cdr2, cdr3, paratope_len, paratope in zip(
+                        df["CDR1"],
+                        df["CDR2"],
+                        df["CDR3"],
+                        df["PARATOPE_LEN"],
+                        df["PARATOPE_DICT_REFORMAT"],
+                    )
+            ]
+        else:
+            for index, probe_dict in final_probe_dict.items():
+                df[f"PARATOPE_MATCH_{index}"] = [
+                    check_paratope_structural(
+                        probe_dict["PARATOPE_LEN"],
+                        probe_dict["PARATOPE_DICT_REFORMAT"],
+                        paratope_len,
+                        paratope,
+                    )
+                    for paratope_len, paratope in zip(
+                        df["PARATOPE_LEN"],
+                        df["PARATOPE_DICT_REFORMAT"],
+                    )
+                ]
+
+        df = pd.concat(
+            [df, nan_df4], ignore_index=False, axis=0, sort=False
+            )
+
+    df = pd.concat(
+        [df, nan_df, nan_df2], ignore_index=False, axis=0, sort=False
+    )
+    df.drop(
+        [
+            "CDR1_NUMBERING",
+            "CDR2_NUMBERING",
+            "CDR3_NUMBERING",
+            "PARATOPE_PROBS",
+            "PARATOPE_PROBS_NUMBERED",
+            "PARATOPE_DICT",
+            "PARATOPE_DICT_NUMBERED",
+            "PARATOPE_DICT_REFORMAT",
+            "LPARATOPE_DICT",
+            "LPARATOPE_DICT_NUMBERED",
+            "LPARATOPE",
+            "CHAIN_TYPE",
+        ],
+        axis=1,
+        inplace=True,
+        errors="ignore",
+    )
+    if both_chains:
+        df.drop(
+            ["CDR1", "CDR2", "CDR3"],
+            axis=1,
+            inplace=True,
+            errors="ignore",
+    )
+
+    print("Another happy probing")
+
+    return df
